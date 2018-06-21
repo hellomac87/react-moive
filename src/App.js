6a0887be52f1b677b099b4b16acda783236b7a2c
hellomac87 <hellomac87@gmail.com>
2018-06-21 10:35:09 +0900

    Lists with .maps

--- src/App.js
+++ src/App.js
@@ -2,27 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-  "Matrix",
-  "Full Metal Jacket",
-  "Oldboy",
-  "Star Wars"
-] 
-const movieImages = [
-  "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg",
-  "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg",
-  "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
-  "https://lh3.googleusercontent.com/7j-fXktVQTihEeJGepheXeVeaHnrFcQ4SwmFb0jxPNj2ceRVKNhBTWoMynp0HyDHrQ53=w200-h300"
+const movies = [
+  {
+    title : "Matrix",
+    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
+  },
+  {
+    title :"Full Metal Jacket",
+    poster:"https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
+  },
+  {
+    title:"Oldboy",
+    poster:"https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+  },
+  {
+    title:"Star Wars",
+    poster:"https://lh3.googleusercontent.com/7j-fXktVQTihEeJGepheXeVeaHnrFcQ4SwmFb0jxPNj2ceRVKNhBTWoMynp0HyDHrQ53=w200-h300"
+  }
 ]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} poster={movieImages[0]}/>
-        <Movie title={movieTitles[1]} poster={movieImages[1]}/>
-        <Movie title={movieTitles[2]} poster={movieImages[2]}/>
-        <Movie title={movieTitles[3]} poster={movieImages[3]}/>
+        {movies.map(movie =>{
+          return <Movie title={movie.title} poster={movie.poster}/>
+        })}
       </div>
     );
   }
